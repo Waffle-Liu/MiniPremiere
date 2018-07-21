fc413cc6a1c96cce148976abad8dbf6af534dfd5
Triwooder <35652386+Triwooder@users.noreply.github.com>
2018-07-21 13:30:56 +0800

    Update model and viewmodel

--- common/Function.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "stdafx.h"
-using namespace std;
-
-int progress(int, int, int);
-string num2str(int);
---
+++ common/Video.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "stdafx.h"
+#include "windowSize.h"
+using namespace std;
+using namespace cv;
+
+typedef vector<shared_ptr<Mat>> VideoFrames;
+class Video
+{
+private:
+	int fCnt;  // frames count
+	double fps;  // frames per second
+	windowSize wSize;  // window size
+	VideoFrames frames;  // frames list 
+
+public:
+	Video();
+	Video(const string &video_path);  // Video(video_path): Capture the video at the destination path.
+	~Video();
+
+	bool capture(const string &video_path);  // this->capture(video_path): Capture the video at the destination path.
+};
