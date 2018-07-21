a82c1ab98aac02a409268b81595685ceb73cb335
Triwooder <35652386+Triwooder@users.noreply.github.com>
2018-07-21 16:33:44 +0800

    Update the model and viewmodel
    function: play

--- VIEWMODEL/prviewmodel.h
+++ VIEWMODEL/prviewmodel.h
@@ -13,11 +13,15 @@ private:
 	shared_ptr<PrModel> model;
 
 	shared_ptr<Command> add_video_command;
+	shared_ptr<Command> play_video_command;
 
 public:
 	PrViewModel();
 	void bind(std::shared_ptr<PrModel> model);
-	void exec_add_video_command(std::string path);
+
+	void exec_add_video_command(string path);
+	void exec_play_video_command(int index);
 
 	shared_ptr<Command> get_add_video_command();
+	shared_ptr<Command> get_play_video_command();
 };
