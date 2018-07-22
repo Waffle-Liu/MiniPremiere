702116d5894167580cf6728cef4d6288a002062a
Waffle-Liu <794227369@qq.com>
2018-07-22 12:05:50 +0800

    update edit
    修改app层 及添加command指令

---
+++ commands/selecteditvideocommand.h
@@ -0,0 +1,18 @@
+#ifndef SELECTEDITVIDEOCOMMAND_H
+#define SELECTEDITVIDEOCOMMAND_H
+
+#include "../common/command.h"
+#include "../VIEWMODEL/prviewmodel.h"
+#include <string>
+
+class SelectEditVideoCommand: public Command
+{
+private:
+    std::shared_ptr<PrViewModel> viewmodel;
+public:
+    SelectEditVideoCommand(std::shared_ptr<PrViewModel> vm);
+    ~SelectEditVideoCommand();
+    virtual void exec();
+};
+
+#endif 
