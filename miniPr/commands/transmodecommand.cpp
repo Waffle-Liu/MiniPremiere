495d70f893b2daa0d921a5a2339144673bb94a08
Waffle-Liu <794227369@qq.com>
2018-07-23 02:36:05 +0800

    debug

--- miniPr/commands/transmodecommand.cpp
+++ miniPr/commands/transmodecommand.cpp
@@ -10,7 +10,6 @@ TransModeCommand::~TransModeCommand()
 
 void TransModeCommand::exec()
 {	
-	int index = std::static_pointer_cast<Int2Parameters, Parameters>(Command::params)->getvalue1();
-	int mode = std::static_pointer_cast<Int2Parameters, Parameters>(Command::params)->getvalue2();
-    viewmodel->exec_trans_mode_command(index, mode);
+	int mode = std::static_pointer_cast<IntParameters, Parameters>(Command::params)->getvalue();
+    viewmodel->exec_trans_mode_command(mode);
 }
