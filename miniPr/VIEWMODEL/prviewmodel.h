5986a312cb961e29e168f9a9109e7eca58c0cc63
Triwooder <35652386+Triwooder@users.noreply.github.com>
2018-07-22 21:05:49 +0800

    integrate notification

--- miniPr/VIEWMODEL/prviewmodel.h
+++ miniPr/VIEWMODEL/prviewmodel.h
@@ -24,6 +24,8 @@ private:
 	shared_ptr<Notification> index_error_notification;
 	shared_ptr<Notification> show_edit_window_notification;
 	shared_ptr<Notification> edit_enable_notification;
+	shared_ptr<Notification> show_integrate_window_notification;
+	shared_ptr<Notification> integrate_complete_notification;
 
 public:
 	PrViewModel();
@@ -43,12 +45,17 @@ public:
 	shared_ptr<Command> get_cut_video_command();
 	shared_ptr<Command> get_speed_change_command();
 	shared_ptr<Command> get_select_edit_video_command();
-	shared_ptr
+	shared_ptr<Command> get_integrate_all_video_command();
 
 	shared_ptr<Notification> get_index_error_notification();
 	shared_ptr<Notification> get_edit_enable_notification();
+	shared_ptr<Notification> get_integrate_complete_notification();
+
 	void set_update_view_notification(shared_ptr<Notification> ntf);
 	void set_show_edit_window_notification(shared_ptr<Notification> ntf);
+	void set_show_integrate_window_notification(shared_ptr<Notification> ntf);
+
 	void update_view_notified(const string &str);
 	void show_edit_window_notified();
+	void show_integrate_window_notified();
 };
